d0ae869f89a1a1aab51f6abdd5375e8aeee069d6
ildar r. khasanshin <ildar.khasanshin@gmail.com>
2016-12-19 21:40:43 +0500

    add disableTitleChangeon

--- mod_jlvkcommunity_messages.php
+++ mod_jlvkcommunity_messages.php
@@ -1,52 +1,38 @@
 <?php
- /**
+/**
  * @package jlvkcommunity_messages
  * @author Kunicin Vadim (vadim@joomline.ru)
  * @version 1.0.0
  * @copyright (C) 2016 by JoomLine (http://www.joomline.net)
  * @license GNU/GPL: http://www.gnu.org/copyleft/gpl.html
  *
-*/
+ */
 
 // No direct access
 defined('_JEXEC') or die('Restricted access');
 
-require_once dirname(__FILE__).'/helper.php';
+require_once dirname(__FILE__) . '/helper.php';
 
-$group_id 					= $params->get('group_id');
-$mode 						= $params->get('mode');
-$link 						= $params->get('link');
-$linknone					= '';
-$disableButtonTooltip 		= $params->get('disableButtonTooltip');
-$tooltipButtonText 			= $params->get('tooltipButtonText');
-$disableNewMessagesSound	= $params->get('disableNewMessagesSound');
-$disableExpandChatSound 	= $params->get('disableExpandChatSound');
-$widgetPosition 			= $params->get('widgetPosition');
-$expanded 					= $params->get('expanded');
+$group_id                = $params->get('group_id');
+$mode                    = $params->get('mode');
+$link                    = $params->get('link');
+$linknone                = '';
+$disableButtonTooltip    = $params->get('disableButtonTooltip');
+$tooltipButtonText       = $params->get('tooltipButtonText');
+$disableNewMessagesSound = $params->get('disableNewMessagesSound');
+$disableExpandChatSound  = $params->get('disableExpandChatSound');
+$disableTitleChange      = $params->get('$disableTitleChange');
+$widgetPosition          = $params->get('widgetPosition');
+$expanded                = $params->get('expanded');
 
+if ($link == 0) {
+  $linknone = '<div style="text-align: right;"><a href="http://afisha-msk.ru/" target="_blank" style="text-decoration:none; color: #c0c0c0; font-family: arial,helvetica,sans-serif; font-size: 5pt; ">afisha-msk.ru</a></div>';
+}
 
-if ($link==0){
-	$linknone = '<div style="text-align: right;"><a href="http://afisha-msk.ru/" target="_blank" style="text-decoration:none; color: #c0c0c0; font-family: arial,helvetica,sans-serif; font-size: 5pt; ">afisha-msk.ru</a></div>';
-	}
+$expandedon                = $expanded == 0 ? '' : ('expandTimeout:' . $expanded . ',');
+$disableButtonTooltipon    = $disableButtonTooltip == 0 ? 'disableButtonTooltip: "1",' : '';
+$disableNewMessagesSoundon = $disableNewMessagesSound == 0 ? 'disableNewMessagesSound: "1",' : '';
+$disableExpandChatSoundon  = $disableExpandChatSound == 0 ? 'disableExpandChatSound: "1",' : '';
+$disableTitleChangeon      = $disableTitleChange == 0 ? 'disableTitleChange: "1",' : '';
 
-if ($expanded==0){
-	$expandedon = '';
-	}
-else {
-	$expandedon = ('expandTimeout:'.$expanded.',');
-	}	
-
-if ($disableButtonTooltip==0){
-	$disableButtonTooltipon = 'disableButtonTooltip: "1",';
-	}
-	
-if ($disableNewMessagesSound==0){
-	$disableNewMessagesSoundon = 'disableNewMessagesSound: "1",';
-	}
-
-	
-if ($disableExpandChatSound==0){
-	$disableExpandChatSoundon = 'disableExpandChatSound: "1",';
-	}
-	
 require JModuleHelper::getLayoutPath('mod_jlvkcommunity_messages', $params->get('layout', 'default'));
