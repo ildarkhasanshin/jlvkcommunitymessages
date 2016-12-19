bdf6ee16fa65dd9a185d10f1117f244bcf2f5585
ildar r. khasanshin <ildar.khasanshin@gmail.com>
2016-12-19 20:41:18 +0500

    spaces

--- tmpl/default.php
+++ tmpl/default.php
@@ -11,11 +11,9 @@
 $doc = JFactory::getDocument();
 $doc->addScript("//vk.com/js/api/openapi.js?136");
 ?>
-
 <div id="jlvk_community_messages<?php echo $group_id;?>">
 <?php echo $linknone;?>
 </div>
 <script type="text/javascript">
 VK.Widgets.CommunityMessages("jlvk_community_messages<?php echo $group_id;?>", <?php echo $group_id;?>, {<?php echo $expandedon;?>widgetPosition: "<?php echo $widgetPosition;?>",<?php echo $disableNewMessagesSoundon;?><?php echo $disableExpandChatSoundon;?><?php echo $disableButtonTooltipon;?> tooltipButtonText: "<?php echo $tooltipButtonText;?>",buttonType: "blue_circle"});
 </script>
-
